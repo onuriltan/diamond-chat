cdb1991f76eb17a5c2b4f6dc95de8274f79464c0
onuriltan <onur.iltan@gmail.com>
2019-03-12 22:38:08 +0300

    remove unused log

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -50,7 +50,6 @@
         FB.login(function (response) {
           if (response.authResponse) {
             this.loginWithFacebook(response.authResponse.accessToken)
-            console.log(response)
           } else {
             console.log('User cancelled login or did not fully authorize.')
           }

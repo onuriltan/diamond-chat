586577f9d8e959bf6c04633dd48bfa077f62fe51
onuriltan <onur.iltan@gmail.com>
2019-04-26 15:54:38 +0300

    add home slogan

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,13 +1,33 @@
 <template>
+  <div class="home">
+    <span class="home__slogan" id="slogans"></span>
+    <div class="home__explanation">
+      Diamond chat is an real time chat application aims to connect people who shares the same music taste.
+    </div>
+
+  </div>
 
 </template>
 
 <script>
-  export default {
-    name: "HomeView"
-  }
+import TypeIt from 'typeit'
+
+export default {
+    name: "HomeView",
+    mounted() {
+        new TypeIt('#slogans', {
+            startDelay: 900,
+            strings: ["Chat with people same with your music taste.", "Use music to connect with people."],
+            speed: 70,
+            loop: true,
+            breakLines: false,
+            waitUntilVisible: true
+        }).go().pause(700);
+    }
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import "../styles/views/homeview";
 
 </style>

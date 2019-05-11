afd555bde4d6db169a43d7ebc8f1fd8bd2233893
onuriltan <onur.iltan@gmail.com>
2019-05-11 20:36:20 +0300

    prism deleted from dashboard page'

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{ applyBackgroundImg }">
     <div id="content">
       <Header />
       <router-view/>
@@ -18,5 +18,40 @@ export default {
     Particles,
     Header,
   },
+  data() {
+      return {
+          applyBackgroundImg : false
+      }
+  },
+  watch: {
+      '$route' (to, from) {
+          to.name !== 'dashboard' ? this.applyBackgroundImg = true : this.applyBackgroundImg = false
+      }
+  }
 };
 </script>
+
+<style lang="scss">
+  .active-page {
+    color: white !important;
+    background-color: black;
+  }
+  .applyBackgroundImg {
+    background: url('~@/assets/ds1.jpg') no-repeat fixed center black;
+  }
+
+  #app {
+    min-height: 100vh;
+    background-size: contain;
+  }
+  #content {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 2;
+    overflow: hidden;
+    font-family: 'Capriola', sans-serif;
+  }
+</style>
--- client/src/views/DashboardView.vue
+++ client/src/views/DashboardView.vue
@@ -14,3 +14,9 @@
     },
   };
 </script>
+
+<style scoped>
+  #app {
+    background: black
+  }
+</style>
